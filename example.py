fdce04b7abc1c49fc76932a3a9b134b6ad1faa09
Laksha Senthilkumar <lakshasenthil@gmail.com>
2023-04-20 14:08:21 -0400

    pseudo

--- example.py
+++ example.py
@@ -1,1 +1,33 @@
 """this is an example"""
+
+"""
+import notify module 
+import OS
+
+createNotif class:
+    creates notification object for every new notification
+    
+addTitle class:
+    adds title to the current notifcation object
+
+addMessage class:
+    adds message to the current notifcation object
+    
+classify class:
+    classifies each notification as a type of alert from local police department
+        - 3 types(emergency, advisory, safety notice)
+    
+icon class:
+    creates an icon for each notifcation (each icon has a different meaning)
+        - icon depends on what's returned from classify class
+sound class:
+    adds sound to each notifcation
+        - sound depends on what's returned from classify class
+
+main function: 
+    calls createNotif class to create new objects for every new notification
+    calls addTitle class to title the new notifcation object
+    calls addMessage class to add message to the new notification object
+    calls icon Class to add iconds to each notifcation object
+    calls sound Class to add sound to each notification object
+    """
