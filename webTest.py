845c34e6585d508f5cf1aecf314c286a78fa1b80
Laksha Senthilkumar <lakshasenthil@gmail.com>
2023-04-25 18:03:33 -0400

    import packages

--- webTest.py
+++ webTest.py
@@ -1,1 +1,8 @@
 "webscraping"
+
+# libraries for getting/retreieving data from the Web
+import urllib.request, urllib.parse, urllib.error
+# library for parsing the data we get from the Web
+import bs4
+import json
+import pandas as pd
